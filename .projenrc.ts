844b2104e43f0cd30577e00c6f2089fdc08b1c25
Court Schuett <schuettc@amazon.com>
2024-01-28 18:06:32 -0600

    fix: first

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,6 +1,7 @@
 const { awscdk } = require('projen');
 const { JobPermission } = require('projen/lib/github/workflows-model');
 const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.118.0',
@@ -57,6 +58,51 @@ project.addTask('getBucket', {
 
 project.addTask('configLocal', {
   exec: 'aws s3 cp s3://$(yarn run --silent getBucket)/config.json site/public/',
+});
+
+const upgradeSite = project.github.addWorkflow('upgrade-site');
+upgradeSite.on({ schedule: [{ cron: '0 0 * * 1' }], workflowDispatch: {} });
+
+upgradeSite.addJobs({
+  upgradeSite: {
+    runsOn: ['ubuntu-latest'],
+    name: 'upgrade-site',
+    permissions: {
+      actions: JobPermission.WRITE,
+      contents: JobPermission.READ,
+      idToken: JobPermission.WRITE,
+    },
+    steps: [
+      { uses: 'actions/checkout@v3' },
+      {
+        name: 'Setup Node.js',
+        uses: 'actions/setup-node@v3',
+        with: {
+          'node-version': '18',
+        },
+      },
+      {
+        run: 'yarn install --check-files --frozen-lockfile',
+        workingDirectory: 'site',
+      },
+      { run: 'yarn upgrade', workingDirectory: 'site' },
+      {
+        name: 'Create Pull Request',
+        uses: 'peter-evans/create-pull-request@v4',
+        with: {
+          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
+          'commit-message': 'chore: upgrade site',
+          'branch': 'auto/projen-upgrade',
+          'title': 'chore: upgrade site',
+          'body': 'This PR upgrades site',
+          'labels': 'auto-merge, auto-approve',
+          'author': 'github-actions <github-actions@github.com>',
+          'committer': 'github-actions <github-actions@github.com>',
+          'signoff': true,
+        },
+      },
+    ],
+  },
 });
 
 project.tsconfigDev.file.addOverride('include', [
